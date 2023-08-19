997f6039644f48cf521959b63976ad158d16e256
haeunNoh06 <okhagrace06@naver.com>
2023-08-19 16:28:43 +0900

    (1.11) 배열 렌더링하기

--- src/UserList.js
+++ src/UserList.js
@@ -28,9 +28,10 @@ function UserList() {
       ];
       return (
         <div>
-            <User user={users[0]} />
-            <User user={users[1]} />
-            <User user={users[2]} />
+            {users.map((user, index) => (
+                <User user={user} key={index}/>// map은 key를 반드시 같이 써야 함
+                // <User user={user} key={user.id}/>// map은 key를 반드시 같이 써야 함
+            ))}
         </div>
       )
 }
