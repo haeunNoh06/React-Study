c9f1b6d73247a31e68cbb629a2b26b2b37da4a0e
haeunNoh06 <okhagrace06@naver.com>
2023-08-19 15:48:33 +0900

    (1.6) 조건부 렌더링

--- src/Hello.js
+++ src/Hello.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Hello({color, name, isSpecial}) {
     return <div style={{color}}>
-        { isSpecial ? <b>*</b> : null }
+        { isSpecial && <b>*</b> }
         안녕하세요. {name}
         </div>
 }
