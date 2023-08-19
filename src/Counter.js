dceb5f25472d1648c9a770e34644af862fcbb625
haeunNoh06 <okhagrace06@naver.com>
2023-08-19 15:58:27 +0900

    (1.7) useState 를 통해 컴포넌트에서 바뀌는 값 관리하기

--- src/Counter.js
+++ src/Counter.js
@@ -2,11 +2,12 @@ import React, {useState} from 'react';
 
 function Counter() {
     const [number, setNumber] = useState(0);
+
     const onIncrease = () => {
-        setNumber(number+1);
+        setNumber(prevNumber => prevNumber + 1);
     }
     const onDecrease = () => {
-        setNumber(number-1);
+        setNumber(prevNumber => prevNumber - 1);
     }
 
     return (
