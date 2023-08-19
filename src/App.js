856cd4a58f2f2457380806e8faf6055678e715c6
haeunNoh06 <okhagrace06@naver.com>
2023-08-19 17:16:00 +0900

    (1.15) 배열 항목 수정하기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 
@@ -7,31 +7,32 @@ function App() {
     username: '',
     email: ''
   });
-
   const { username, email } = inputs;
   const onChange = e => {
-    const {name, value} = e.target;
+    const { name, value } = e.target;
     setInputs({
       ...inputs,
       [name]: value
     });
   };
-
   const [users, setUsers] = useState([
     {
       id: 1,
       username: 'velopert',
-      email: 'public.velopert@gmail.com'
+      email: 'public.velopert@gmail.com',
+      active: true
     },
     {
       id: 2,
       username: 'tester',
-      email: 'tester@example.com'
+      email: 'tester@example.com',
+      active: false
     },
     {
       id: 3,
       username: 'liz',
-      email: 'liz@example.com'
+      email: 'liz@example.com',
+      active: false
     }
   ]);
 
@@ -47,24 +48,26 @@ function App() {
     setInputs({
       username: '',
       email: ''
-    })
-    nextId.current++;
+    });
+    nextId.current += 1;
   };
 
   const onRemove = id => {
-    // user.id가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듦
-    // = user.id 가 id인것을 제거함
+    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // = user.id 가 id 인 것을 제거함
     setUsers(users.filter(user => user.id !== id));
   };
+
+  
   return (
     <>
-      <CreateUser 
-      username={username}
-      email={email}
-      onChange={onChange}
-      onCreate={onCreate}
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemove}/>
+      <UserList users={users} onRemove={onRemove} />
     </>
   );
 }
--- src/UserList.js
+++ src/UserList.js
@@ -1,24 +1,33 @@
 import React from 'react';
 
-function User({user, onRemove}) {
+function User({ user, onRemove, onToggle }) {
     return (
         <div>
-            <b>{user.username}</b> <span>({user.email})</span>
+            <b
+                style={{
+                    cursor: 'pointer',
+                    color: user.active ? 'green' : 'black'
+                }}
+            >
+                {user.username}
+            </b>
+
+            <span>({user.email})</span>
             <button onClick={() => onRemove(user.id)}>삭제</button>
         </div>
     );
 }
 
-function UserList({users, onRemove}) {
-    
-      return (
+function UserList({ users, onRemove, onToggle }) {
+
+    return (
         <div>
             {users.map(user => (
-                <User user={user} key={user.id} onRemove={onRemove}/>// map은 key를 반드시 같이 써야 함
+                <User user={user} key={user.id} onRemove={onRemove} />// map은 key를 반드시 같이 써야 함
                 // <User user={user} key={user.id}/>// map은 key를 반드시 같이 써야 함
             ))}
         </div>
-      )
+    );
 }
 
 export default UserList;
